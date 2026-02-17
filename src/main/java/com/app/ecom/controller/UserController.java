f46502ff25530c36fc73c979807737dc6432b8bc
Hao Xu <haoxugt@gmail.com>
2026-02-17 11:19:19 -0800

    Add UserResponse, UserRequest, AddressDTO for DTOs, and migrate UserController and UserService to use DTOs

--- src/main/java/com/app/ecom/controller/UserController.java
+++ src/main/java/com/app/ecom/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.app.ecom.controller;
 
-import com.app.ecom.model.User;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
 import com.app.ecom.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,34 +18,28 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
         return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getAUsers(@PathVariable Long id) {
-//        User user =  userService.fetchUser(id);
-//        if (user != null) {
-//            return new ResponseEntity<>(user, HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(user, HttpStatus.NOT_FOUND);
-//        }
+    public ResponseEntity<UserResponse> getAUsers(@PathVariable Long id) {
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        userService.addUser(user);
+    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
+        userService.addUser(userRequest);
         return ResponseEntity.ok("User added successful");
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@PathVariable Long id,
-                                             @RequestBody User updatedUser) {
-        boolean updated = userService.updateUser(id, updatedUser);
+                                             @RequestBody UserRequest updatedUserRequest) {
+        boolean updated = userService.updateUser(id, updatedUserRequest);
         if (updated)
             return ResponseEntity.ok("User updated successfully");
         return ResponseEntity.notFound().build();
--- src/main/java/com/app/ecom/service/UserService.java
+++ src/main/java/com/app/ecom/service/UserService.java
@@ -1,5 +1,9 @@
 package com.app.ecom.service;
 
+import com.app.ecom.dto.AddressDTO;
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
+import com.app.ecom.model.Address;
 import com.app.ecom.model.User;
 import com.app.ecom.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,28 +16,71 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-//    private List<User> userList = new ArrayList<>();
-//    private Long nextId = 1L;
 
-    public List<User> fetchAllUsers() {
-        return userRepository.findAll();
+    public List<UserResponse> fetchAllUsers() {
+        return userRepository.findAll().stream()
+                .map(this::mapToUserResponse)
+                .toList();
     }
 
-    public void addUser(User user) {
+    public void addUser(UserRequest userRequest) {
+        User user = new User();
+        updateUserFromRequest(user, userRequest);
         userRepository.save(user);
     }
 
-    public Optional<User> fetchUser(Long id) {
-        return userRepository.findById(id);
+    public Optional<UserResponse> fetchUser(Long id) {
+        return userRepository.findById(id)
+                .map(this::mapToUserResponse);
     }
 
-    public boolean updateUser(Long id, User updatedUser) {
+    public boolean updateUser(Long id, UserRequest updatedUserRequest) {
         return userRepository.findById(id)
                 .map(existingUser -> {
-                    existingUser.setFirstName(updatedUser.getFirstName());
-                    existingUser.setLastName(updatedUser.getLastName());
+                    updateUserFromRequest(existingUser, updatedUserRequest);
                     userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
+    }
+
+    // DTO transfer
+    private UserResponse mapToUserResponse(User user){
+        UserResponse response = new UserResponse();
+        response.setId(String.valueOf(user.getId()));
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setEmail(user.getEmail());
+        response.setPhoneNumber(user.getPhoneNumber());
+        response.setRole(user.getRole());
+
+        if (user.getAddress() != null) {
+            AddressDTO addressDTO = new AddressDTO();
+            addressDTO.setStreet(user.getAddress().getStreet());
+            addressDTO.setCity(user.getAddress().getCity());
+            addressDTO.setState(user.getAddress().getState());
+            addressDTO.setCountry(user.getAddress().getCountry());
+            addressDTO.setZipcode(user.getAddress().getZipcode());
+            response.setAddress(addressDTO);
+        }
+
+        return response;
+
+    }
+
+    private void updateUserFromRequest(User user, UserRequest userRequest) {
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setEmail(userRequest.getEmail());
+        user.setPhoneNumber(userRequest.getPhoneNumber());
+
+        if (userRequest.getAddress() != null) {
+            Address address = new Address();
+            address.setStreet(userRequest.getAddress().getStreet());
+            address.setCity(userRequest.getAddress().getCity());
+            address.setState(userRequest.getAddress().getState());
+            address.setCountry(userRequest.getAddress().getCountry());
+            address.setZipcode(userRequest.getAddress().getZipcode());
+            user.setAddress(address);
+        }
     }
 }
