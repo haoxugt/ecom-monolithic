d7b6ce491354cd9e1bb8f8546d502120d3f5f80d
Hao Xu <haoxugt@gmail.com>
2026-02-18 15:42:50 -0800

    Add Endpoint to fetch Cart

--- src/main/java/com/app/ecom/controller/CartController.java
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -1,11 +1,14 @@
 package com.app.ecom.controller;
 
 import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.model.CartItem;
 import com.app.ecom.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +27,11 @@ public class CartController {
                     .body("Product Out of Stock or User not found or Product not found");
         }
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getCart(@RequestHeader("X-User-ID") String userId){
+        return ResponseEntity.ok(cartService.getCart(userId));
     }
 
     @DeleteMapping("/items/{productId}")
--- src/main/java/com/app/ecom/repository/CartItemRepository.java
+++ src/main/java/com/app/ecom/repository/CartItemRepository.java
@@ -6,9 +6,12 @@ import com.app.ecom.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByUserAndProduct(User user, Product product);
 
     void deleteByUserAndProduct(User user, Product product);
+
 }
